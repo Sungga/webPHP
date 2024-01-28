8cf28dc430d855e3841017f17bc93a74e2a0e939
Sungga <anhvu17003@gmail.com>
2024-01-28 10:10:06 +0700

    update

--- access/js/js_list_product.js
+++ access/js/js_list_product.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     // ----------------<< setting an hien thanh cong cu >>----------------
-    var itemTops = document.querySelectorAll('.list-product__left--item-top');
+    let itemTops = document.querySelectorAll('.list-product__left--item-top');
 
     itemTops.forEach(function(itemTop, index) {
         itemTop.addEventListener("click", function() {
-            var itemBottom = itemTop.nextElementSibling; // lay phan tu ke tiep
+            let itemBottom = itemTop.nextElementSibling; // lay phan tu ke tiep
 
             // toggle hien thi/an itemBottom
             if (itemBottom.style.display === 'flex') {
@@ -19,11 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+
     // ---------------<< setting thanh truot muc gia >>-------------------
-    var range = document.getElementById('myRange');
-    var rangeValue = document.getElementById('rangeValue');
+    let range = document.getElementById('myRange');
+    let rangeValue = document.getElementById('rangeValue');
 
     range.addEventListener('input', function() {
         rangeValue.textContent = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(range.value);
     });
+
+
+    // -----------------<< setting hien thi chon size khi an vao gio hang >>--------------------
+    let cartItems = document.querySelectorAll('.list-product__product--cart');
+
+    cartItems.forEach(cartItem => {
+    cartItem.addEventListener('click', function() {
+        // Lấy phần tử cartSize tương ứng (trong cùng một container hoặc có cách xác định phù hợp)
+        let relatedCartSize = this.closest('.list-product__product--cart').querySelector('.list-product__product--size');
+
+        // Kiểm tra trạng thái hiện tại của cartSize và đảo ngược giá trị display
+        if (relatedCartSize.style.visibility === 'inherit') {
+            relatedCartSize.style.visibility = 'hidden';
+            relatedCartSize.style.opacity = '0';
+        } else {
+            relatedCartSize.style.opacity = '1';
+            relatedCartSize.style.visibility = 'inherit';
+        }
+    });
+    });
+
 });
